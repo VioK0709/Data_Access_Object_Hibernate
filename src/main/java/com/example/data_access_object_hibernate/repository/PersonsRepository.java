0474b31cba64f3bae5b5f96fecac5c3ada773cc6
Violetta Korolkova <la_bella.vita@mail.ru>
2023-08-29 17:39:20 +0200

    JpaRepository

--- src/main/java/com/example/data_access_object_hibernate/repository/PersonsRepository.java
+++ src/main/java/com/example/data_access_object_hibernate/repository/PersonsRepository.java
@@ -3,7 +3,6 @@ package com.example.data_access_object_hibernate.repository;
 import com.example.data_access_object_hibernate.entity.Persons;
 import com.example.data_access_object_hibernate.entity.PersonsId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,12 +11,9 @@ import java.util.Optional;
 @Repository
 public interface PersonsRepository extends JpaRepository<Persons, PersonsId> {
 
-    @Query(nativeQuery = true, value = "select * from persons where city = :city")
     List<Persons> findByCity(String city);
 
-    @Query(nativeQuery = true, value = "select * from persons where age < :age ORDER BY age ASC ")
     List<Persons> findByAge(Integer age);
 
-    @Query(nativeQuery = true, value = "select * from persons where name, surname =: name, surname")
     Optional<Persons> findByNameAndSurname(String name, String surname);
 }
