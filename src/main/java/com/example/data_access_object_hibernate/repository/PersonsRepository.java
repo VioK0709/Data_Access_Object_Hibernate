122de30eb211e32324a6f61d51f5ee8a47dad86d
Violetta Korolkova <113035992+VioK0709@users.noreply.github.com>
2023-08-30 14:59:40 +0200

    Update PersonsRepository.java

--- src/main/java/com/example/data_access_object_hibernate/repository/PersonsRepository.java
+++ src/main/java/com/example/data_access_object_hibernate/repository/PersonsRepository.java
@@ -17,6 +17,6 @@ public interface PersonsRepository extends JpaRepository<Persons, PersonsId> {
     @Query("select p from Persons p where p.age < :age ORDER BY p.age ASC ")
     List<Persons> findByAgeLessThanOrderByAge(Integer age);
 
-    @Query("select p from Persons p where p.name =:nane and p.surname =: surname")
+    @Query("select p from Persons p where p.name = :name and p.surname = :surname")
     Optional<Persons> findByNameAndSurname(String name, String surname);
 }
