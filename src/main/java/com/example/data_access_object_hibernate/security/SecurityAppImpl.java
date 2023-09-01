99ee14b7fb3d392c84203235781cd211627834ee
Violetta Korolkova <la_bella.vita@mail.ru>
2023-09-01 09:13:22 +0200

    Spring Security

--- src/main/java/com/example/data_access_object_hibernate/security/SecurityAppImpl.java
+++ src/main/java/com/example/data_access_object_hibernate/security/SecurityAppImpl.java
@@ -3,12 +3,16 @@ package com.example.data_access_object_hibernate.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityAppImpl implements WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityAppImpl {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -28,12 +32,16 @@ public class SecurityAppImpl implements WebSecurityConfigurerAdapter {
         return manager;
     }
 
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((authorize) ->
-                authorize.requestMatchers("/hello")
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/hello")
                         .permitAll()
                         .anyRequest()
                         .authenticated());
+
+        return httpSecurity.build();
     }
 }
--- src/main/java/com/example/data_access_object_hibernate/security/WebSecurityConfigurerAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.example.data_access_object_hibernate.security;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-public interface WebSecurityConfigurerAdapter {
-
-    void configure(HttpSecurity httpSecurity) throws Exception;
-}
