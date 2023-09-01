e46966b197fc14acae9fc92150f26374d32345b4
Violetta Korolkova <la_bella.vita@mail.ru>
2023-09-01 12:02:17 +0200

    Spring Security

--- src/main/java/com/example/data_access_object_hibernate/security/SecurityApp.java
+++ src/main/java/com/example/data_access_object_hibernate/security/SecurityApp.java
@@ -40,7 +40,6 @@ public class SecurityApp {
                         .permitAll()
                         .anyRequest()
                         .authenticated());
-
         return null;
     }
 }
